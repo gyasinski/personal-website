c85231e96c2bc52fa30189e608379a216023e25a
Greg Yasinski <75457435+gyasinski@users.noreply.github.com>
2023-03-21 02:04:40 -0400

    Started creating front end in React
    - Imported react-awesome-shapes
    - Imported motion-framer
    - Started working on background and star generation

---
+++ README.txt
@@ -0,0 +1,16 @@
+To start the backend:
+
+	1.) cd flask-server
+
+	2.) python -m venv venv
+
+	3.) .\venv\Scripts\activate
+
+	4.) python server.py
+
+
+To start the front end:
+
+	1.) cd website-frontend
+
+	2.) npm start
--- website-frontend/src/App.js
+++ website-frontend/src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
+import { Star } from 'react-awesome-shapes';
 
 function App() {
   return (
       <div className="App">
-          <p>Hello, my name is Greg</p>
+          <BuildStars />
+          <div className="square">
+            <h1>Hello, my name is Greg</h1>
+          </div>
       </div>
   );
+}
+
+function BuildStars() {
+    let RandomY, RandomX, RandomSize;
+    const stars = [];
+
+    for (let i = 0; i < 1000; i++) {
+        RandomY = Math.floor(Math.random()* window.innerHeight).toString() + "px";
+        RandomX = Math.floor(Math.random()* window.innerWidth).toString() + "px";
+        RandomSize = Math.floor((Math.random()*5) + 5).toString() + "px";
+
+        stars.push(
+            <Star
+                color="#FFFFFF"
+                size={RandomSize}
+                top={RandomY}
+                left={RandomX}
+                zIndex={0}
+            />
+        )
+    }
+    return (stars)
 }
 
 export default App;
