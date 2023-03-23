5b13cea89b777a2b374616f4d77c660e4c35056c
Greg Yasinski <75457435+gyasinski@users.noreply.github.com>
2023-03-23 00:00:19 -0400

    updated css
    - glass divs now position in a more reliable fashion by switching to viewport units instead of percentages and fixing a bug where i used vh for the width property

--- website-frontend/src/App.js
+++ website-frontend/src/App.js
@@ -7,14 +7,19 @@ function App() {
           <BuildStars />
           <div className="toolBar">
               <div className="toolButtons">
-                  <button className="toolButton" type="button">Experience</button>
                   <button className="toolButton" type="button">Projects</button>
+                  <button className="toolButton" type="button">Experience</button>
                   <button className="toolButton" type="button">Contact Me</button>
               </div>
           </div>
           <div className="screen">
               <div className="glass1">
                   <p>Hi im Greg</p>
+              </div>
+          </div>
+          <div className="screen">
+              <div className="glass2">
+                  <p>Projects</p>
               </div>
           </div>
       </div>
@@ -25,13 +30,14 @@ function BuildStars() {
     let RandomY, RandomX, RandomSize;
     const stars = [];
 
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 500; i++) {
         RandomY = Math.floor(Math.random()* window.innerHeight).toString() + "px";
         RandomX = Math.floor(Math.random()* window.innerWidth).toString() + "px";
         RandomSize = Math.floor((Math.random()*5) + 5).toString() + "px";
 
         stars.push(
             <Star
+                position="fixed"
                 color="#FFFFFF"
                 size={RandomSize}
                 top={RandomY}
